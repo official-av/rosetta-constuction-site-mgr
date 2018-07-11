04c27e295d685b59c21d01549a9640194dd5e3d2
Anmol Vashistha <anmolvashistha@hotmail.com>
2018-07-11 20:28:14 +0530

    added ng material + flex layout + basic setup

---
+++ src/app/shared.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {MaterialModule} from './material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+@NgModule({
+  imports: [
+    MaterialModule,
+    FlexLayoutModule
+  ],
+  exports: []
+})
