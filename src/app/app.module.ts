d9cabb5f3051b690bf58a7f1fe33ddd0b9ddf1d4
Anmol Vashistha <anmol.vashistha@newgen.co.in>
2018-10-03 12:40:14 +0530

    added spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {SharedModule} from './shared.module';
-import {CoreModule} from './core/core.module';
-import {RouterModule} from '@angular/router';
-import {RoutesModule} from './routes.module';
-import {DatePipe} from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared.module';
+import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
+import { RoutesModule } from './routes.module';
+import { DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgHttpLoaderModule } from 'ng-http-loader';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,17 @@ import {DatePipe} from '@angular/common';
   imports: [
     BrowserModule,
     RouterModule,
+    HttpClientModule,
     /*custom modules*/
     BrowserAnimationsModule,
     SharedModule,
     CoreModule,
-    RoutesModule
+    RoutesModule,
+    NgHttpLoaderModule
   ],
-  providers: [DatePipe],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 
